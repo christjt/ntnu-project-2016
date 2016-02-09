6a763bbb56a94b657f9cd8b3c17c48329949c89e
Eirik Jakobsen <eirikjak@gmail.com>
2016-02-09 15:30:38 +0100

    smaller allowed position offset

--- project-code/roborobo2/src/ext/ConnectionPort.cpp
+++ project-code/roborobo2/src/ext/ConnectionPort.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-const double _collisionPositionOffset = 5;
+const double _collisionPositionOffset = 1;
 const double _collisionOrientationOffset = 1.0;
 const double PERFECT_ORIENTATIONAL_FIT = 180.0;
 
