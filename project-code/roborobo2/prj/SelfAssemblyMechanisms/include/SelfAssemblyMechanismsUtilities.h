3deb348ce60a10d9028bf89bdeb80b2592c132c8
christjt <christannum@gmail.com>
2016-02-19 16:20:08 +0100

    Add utilities class

---
+++ project-code/roborobo2/prj/SelfAssemblyMechanisms/include/SelfAssemblyMechanismsUtilities.h
@@ -0,0 +1,15 @@
+//
+// Created by christopher on 19.02.16.
+//
+
+#ifndef ROBOROBO2_SELFASSEMBLYMECHANISMSUTILITIES_H
+#define ROBOROBO2_SELFASSEMBLYMECHANISMSUTILITIES_H
+
+
+class SelfAssemblyMechanismsUtilities {
+    public:
+        static double normalizeDouble(double x, double minFrom, double maxFrom, double minTo, double maxTo);
+};
+
+
+#endif //ROBOROBO2_SELFASSEMBLYMECHANISMSUTILITIES_H
