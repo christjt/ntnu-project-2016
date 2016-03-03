1b96ae51708dadf711c687ca8da4e06804fb6eb9
Eirik Jakobsen <eirikjak@gmail.com>
2016-03-03 16:04:01 +0100

    disable show results when training

--- project-code/roborobo2/run_training.py
+++ project-code/roborobo2/run_training.py
@@ -6,7 +6,7 @@ parser.optionxform = str
 parser.read('config/default-self_assembly_mechanisms.properties')
 parser.set('config', 'gBatchMode', 'true')
 parser.set('config', 'gDisplayMode', '2')
-parser.set('config', 'gDisplayBestGenome', 'true')
+parser.set('config', 'gDisplayBestGenome', 'false')
 with open('config/generated-self_assembly_mechanisms.properties', 'w') as configFile:
     parser.write(configFile)
 os.system('mpirun  -np %s ./roborobo -l config/generated-self_assembly_mechanisms.properties' % sys.argv[1])
